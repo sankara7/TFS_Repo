77c1f26d49f98b08800a157c9550e1d7d5b4a978
sankara7 <30866209+sankara7@users.noreply.github.com>
2017-10-30 14:48:22 +0530

    Add files via upload
    PowerShell install script

---
+++ Install.ps1
@@ -0,0 +1,22 @@
+﻿
+# Below block will downloaded ManageEngine installer from official site into tmp path if not downloaded already
+if (!(Test-Path c:\ManageEngine32.exe)) {
+
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://www.manageengine.com/cgi-bin/download_exe?id=1-33","C:\ManageEngine32.exe")
+
+}
+
+# Below block will download the installation paramater setup file from Github
+if (!(Test-Path C:\param.iss)) {
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://raw.githubusercontent.com/sankara7/TFS_Repo/master/param.iss","C:\param.iss")
+}
+
+# Below command will start the installation process of ManageEngine application manager in slient mode using param.iss paramter file
+
+Start-Process "C:\ManageEngine32.exe" -ArgumentList "/s /a /s /sms /f1c:\param.iss" -Wait -NoNewWindow
+
+
+
+
