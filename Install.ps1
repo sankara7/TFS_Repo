8c6739203190d6b51fabf16a1eabeb2eb1950a76
sankara7 <30866209+sankara7@users.noreply.github.com>
2017-10-30 16:22:45 +0530

    Update Install.ps1
    Changed the installer to 64 bit

--- Install.ps1
+++ Install.ps1
@@ -1,9 +1,9 @@
 ﻿
 # Below block will downloaded ManageEngine installer from official site into tmp path if not downloaded already
-if (!(Test-Path c:\ManageEngine32.exe)) {
+if (!(Test-Path c:\ManageEngine64.exe)) {
 
 $WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://www.manageengine.com/cgi-bin/download_exe?id=1-33","C:\ManageEngine32.exe")
+$WebClient.DownloadFile("https://www.manageengine.com/cgi-bin/download_exe?id=1-918","C:\ManageEngine64.exe")
 
 }
 
@@ -15,7 +15,7 @@ $WebClient.DownloadFile("https://raw.githubusercontent.com/sankara7/TFS_Repo/mas
 
 # Below command will start the installation process of ManageEngine application manager in slient mode using param.iss paramter file
 
-Start-Process "C:\ManageEngine32.exe" -ArgumentList "/s /a /s /sms /f1c:\param.iss" -Wait -NoNewWindow
+Start-Process "C:\ManageEngine64.exe" -ArgumentList "/s /a /s /sms /f1c:\param.iss" -Wait -NoNewWindow
 
 
 
