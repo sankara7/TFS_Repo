238c847505b6c604b3a26050059b1ea9a5e858b6
sankara7 <30866209+sankara7@users.noreply.github.com>
2017-10-30 16:25:26 +0530

    Update Install.ps1
    Changed the setup file to 64 bit

--- Install.ps1
+++ Install.ps1
@@ -10,12 +10,12 @@ $WebClient.DownloadFile("https://www.manageengine.com/cgi-bin/download_exe?id=1-
 # Below block will download the installation paramater setup file from Github
 if (!(Test-Path C:\param.iss)) {
 $WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://raw.githubusercontent.com/sankara7/TFS_Repo/master/param.iss","C:\param.iss")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/sankara7/TFS_Repo/master/setup.iss","C:\setup.iss")
 }
 
 # Below command will start the installation process of ManageEngine application manager in slient mode using param.iss paramter file
 
-Start-Process "C:\ManageEngine64.exe" -ArgumentList "/s /a /s /sms /f1c:\param.iss" -Wait -NoNewWindow
+Start-Process "C:\ManageEngine64.exe" -ArgumentList "/s /a /s /sms /f1c:\setup.iss" -Wait -NoNewWindow
 
 
 
