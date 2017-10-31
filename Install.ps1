e26b2880412a6c3a3d1a184724b75727982b84b9
sankara7 <30866209+sankara7@users.noreply.github.com>
2017-10-31 17:28:57 +0530

    Update Install.ps1
    Changed the code structure in functions

--- Install.ps1
+++ Install.ps1
@@ -1,21 +1,47 @@
-﻿
+$log = "c:\log.txt"
+function download
+{
+try
+{
 # Below block will downloaded ManageEngine installer from official site into tmp path if not downloaded already
 if (!(Test-Path c:\ManageEngine64.exe)) {
 
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://www.manageengine.com/cgi-bin/download_exe?id=1-918","C:\ManageEngine64.exe")
-
+Add-Content -Path $Log -value "ManageEngine Downloaded"
 }
 
 # Below block will download the installation paramater setup file from Github
-if (!(Test-Path C:\setup.iss)) {
+if (!(Test-Path C:\set.iss)) {
 $WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://raw.githubusercontent.com/sankara7/TFS_Repo/master/setup.iss","C:\setup.iss")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/sankara7/TFS_Repo/master/set.iss","C:\set.iss")
+Add-Content -Path $Log -value "Setup file Downloaded"
+}
+}
+catch
+{
+
+}
+}
+function install
+{
+try
+{
+Start-Process "C:\ManageEngine64.exe" -ArgumentList "/s /a /s /sms /f1c:\set.iss" -Wait -NoNewWindow
+}
+catch
+{
+}
 }
 
-# Below command will start the installation process of ManageEngine application manager in slient mode using param.iss paramter file
-
-Start-Process "C:\ManageEngine64.exe" -ArgumentList "/s /a /s /sms /f1c:\setup.iss" -Wait -NoNewWindow
+Add-Content -Path $Log -value "Downloads Started"
+$status = download
+if($?)
+{
+Add-Content -Path $Log -value "Install Started"
+install
+Add-Content -Path $Log -value "Install Finshed"
+}
 
 
 
