7539da3ba8a458360659d9968013635e17c865b2
sankara7 <30866209+sankara7@users.noreply.github.com>
2017-11-03 17:33:21 +0530

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -8,14 +8,14 @@ $WebClient.DownloadFile("https://www.manageengine.com/cgi-bin/download_exe?id=1-
 }
 
 # Below block will download the installation paramater setup file from Github
-if (!(Test-Path C:\set.iss)) {
+if (!(Test-Path C:\setup.iss)) {
 $WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://github.com/sankara7/TFS_Repo/blob/ARM_Branch/set.iss","C:\set.iss")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/sankara7/TFS_Repo/master/setup.iss","C:\setup.iss")
 }
 
 # Below command will start the installation process of ManageEngine application manager in slient mode using param.iss paramter file
 
-Start-Process "C:\ManageEngine.exe" -ArgumentList "/s /a /s /sms /f1c:\set.iss" -Wait -NoNewWindow
+Start-Process "c:\ManageEngine.exe" -ArgumentList "/s /a /s /sms /f1c:\setup.iss" -Wait -NoNewWindow
 
 
 
