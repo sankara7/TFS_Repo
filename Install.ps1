c4a7e1912a458c9820180e1c2da1d1203d874412
sankara7 <30866209+sankara7@users.noreply.github.com>
2017-11-03 18:57:04 +0530

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -1,6 +1,8 @@
+Set-ExecutionPolicy Unrestricted -Force
 try
 {
 # Below block will downloaded ManageEngine installer from official site into tmp path if not downloaded already
+Write-Host "download exe started"
 Start-Job -Name job1 -ScriptBlock {
 if (!(Test-Path c:\ManageEngine.exe)) {
 
@@ -10,8 +12,10 @@ $WebClient.DownloadFile("https://www.manageengine.com/cgi-bin/download_exe?id=1-
 }
 }
 Wait-Job -Name job1
+Write-Host "download exe completed"
 
 # Below block will download the installation paramater setup file from Github
+Write-Host "download param started"
 Start-Job -Name job2 -ScriptBlock {
 if (!(Test-Path C:\setup.iss)) {
 $WebClient = New-Object System.Net.WebClient
@@ -19,6 +23,7 @@ $WebClient.DownloadFile("https://raw.githubusercontent.com/sankara7/TFS_Repo/mas
 }
 }
 Wait-Job -Name job2
+Write-Host "download param completed"
 }
 Catch
 {
@@ -30,11 +35,14 @@ $Time=Get-Date
 
 try
 {
+Write-Host "install started"
 Start-Job -Name job3 -ScriptBlock {
 Set-ExecutionPolicy Unrestricted -Force
 Start-Process c:\ManageEngine.exe -ArgumentList '/quiet /a /s /sms /f1c:\setup.iss' -Wait
 Wait-Job -Name job3
+Write-Host "instal completed"
 }
+
 }
 Catch
 {
@@ -42,6 +50,3 @@ $ErrorMessage = $_.Exception.Message
 $Time=Get-Date
 "This script failed at $Time and error message was $ErrorMessage" | out-file c:\log.txt -append
 }
-
-
-
