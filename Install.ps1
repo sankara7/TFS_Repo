8ac0016a97773498452c5c059d7b9c17f8a0f5a2
sankara7 <30866209+sankara7@users.noreply.github.com>
2017-11-03 18:33:16 +0530

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -1,21 +1,27 @@
-﻿
+
 # Below block will downloaded ManageEngine installer from official site into tmp path if not downloaded already
-$proc = if (!(Test-Path c:\ManageEngine.exe)) {
+Start-Job -Name job1 -ScriptBlock {
+if (!(Test-Path c:\ManageEngine.exe)) {
 
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://www.manageengine.com/cgi-bin/download_exe?id=1-918","C:\ManageEngine.exe")
 
 }
-$proc.WaitForExit() 
+}
+Wait-Job -Name job1
 
 # Below block will download the installation paramater setup file from Github
-$proc = if (!(Test-Path C:\setup.iss)) {
+Start-Job -Name job2 -ScriptBlock {
+if (!(Test-Path C:\setup.iss)) {
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://raw.githubusercontent.com/sankara7/TFS_Repo/master/setup.iss","C:\setup.iss")
 }
-$proc.WaitForExit()
+}
+Wait-Job -Name job2
 
 # Below command will start the installation process of ManageEngine application manager in slient mode using param.iss paramter file
+
+Start-Job -Name job3 -ScriptBlock {
 try
 {
 Set-ExecutionPolicy Unrestricted -Force
@@ -27,8 +33,5 @@ $ErrorMessage = $_.Exception.Message
 $Time=Get-Date
 "This script failed at $Time and error message was $ErrorMessage" | out-file c:\log.txt -append
 }
-
-
-
-
-
+}
+Wait-Job -Name job3
