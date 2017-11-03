9e883372200f6998a424e9d469747830a6a302a0
sankara7 <30866209+sankara7@users.noreply.github.com>
2017-11-03 16:30:28 +0530

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -1,21 +1,21 @@
 ﻿
 # Below block will downloaded ManageEngine installer from official site into tmp path if not downloaded already
-if (!(Test-Path c:\tmp\ManageEngine32.exe)) {
+if (!(Test-Path c:\ManageEngine.exe)) {
 
 $WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://www.manageengine.com/cgi-bin/download_exe?id=1-33","C:\tmp\ManageEngine32.exe")
+$WebClient.DownloadFile("https://www.manageengine.com/cgi-bin/download_exe?id=1-918","C:\ManageEngine.exe")
 
 }
 
 # Below block will download the installation paramater setup file from Github
-if (!(Test-Path C:\tmp\set.iss)) {
+if (!(Test-Path C:\set.iss)) {
 $WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://raw.githubusercontent.com/sankara7/TFS_Repo/ARM_Branch/set.iss","C:\tmp\set.iss")
+$WebClient.DownloadFile("https://github.com/sankara7/TFS_Repo/blob/ARM_Branch/set.iss","C:\set.iss")
 }
 
 # Below command will start the installation process of ManageEngine application manager in slient mode using param.iss paramter file
 
-Start-Process "C:\tmp\ManageEngine32.exe" -ArgumentList "/s /a /s /sms /f1c:\tmp\set.iss" -Wait -NoNewWindow
+Start-Process "C:\ManageEngine.exe" -ArgumentList "/s /a /s /sms /f1c:\set.iss" -Wait -NoNewWindow
 
 
 
