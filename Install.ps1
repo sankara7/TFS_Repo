4682e8e33136721391ef3dd4edd9e257583320fb
sankara7 <30866209+sankara7@users.noreply.github.com>
2017-11-03 18:20:53 +0530

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -1,17 +1,19 @@
 ﻿
 # Below block will downloaded ManageEngine installer from official site into tmp path if not downloaded already
-if (!(Test-Path c:\ManageEngine.exe)) {
+$proc = if (!(Test-Path c:\ManageEngine.exe)) {
 
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://www.manageengine.com/cgi-bin/download_exe?id=1-918","C:\ManageEngine.exe")
 
 }
+$proc.WaitForExit() 
 
 # Below block will download the installation paramater setup file from Github
-if (!(Test-Path C:\setup.iss)) {
+$proc = if (!(Test-Path C:\setup.iss)) {
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://raw.githubusercontent.com/sankara7/TFS_Repo/master/setup.iss","C:\setup.iss")
 }
+$proc.WaitForExit()
 
 # Below command will start the installation process of ManageEngine application manager in slient mode using param.iss paramter file
 try
