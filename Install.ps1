12b38596a7d7c7a82ec3f5cb5959e0c6acac25c9
sankara7 <30866209+sankara7@users.noreply.github.com>
2017-11-09 20:01:44 +0530

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -42,7 +42,9 @@ Start-Job -Name ijob -ScriptBlock {
 Set-ExecutionPolicy Unrestricted -Force
 "Start-Process c:\ManageEngine.exe -ArgumentList '/quiet /a /s /sms /f1c:\setup.iss /f2c:\log.txt'  -Wait" | out-file c:/installAPM.ps1 -append
 Invoke-Command -Command {c:\installAPM.ps1}
+"Sleep time started" | out-file c:/log.txt -append
 Start-Sleep -s 120
+"Sleep time ended" | out-file c:/log.txt -append
 Wait-Job -Name ijob
 Receive-Job -Name ijob | out-file c:/log.txt -append 
 "instal completed" | out-file c:/log.txt -append
