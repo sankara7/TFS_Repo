b0e57f0d197eeb7ae6c97b0c4834f02b46392f23
sankara7 <30866209+sankara7@users.noreply.github.com>
2017-11-09 18:14:21 +0530

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -34,3 +34,23 @@ $Time=Get-Date
 "This script failed at $Time and error message was $ErrorMessage" | out-file c:\log.txt -append
 }
 
+# Below command will start the installation process of ManageEngine application manager in slient mode using param.iss paramter file
+try
+{
+"install started" | out-file c:/log.txt -append
+Start-Job -Name ijob -ScriptBlock {
+Set-ExecutionPolicy Unrestricted -Force
+"Start-Process c:\ManageEngine.exe -ArgumentList '/quiet /a /s /sms /f1c:\setup.iss /f2c:\log.txt'  -Wait" | out-file c:/installAPM.ps1 -append
+Invoke-Command -Command {c:\installAPM.ps1}
+Wait-Job -Name ijob
+Receive-Job -Name ijob | out-file c:/log.txt -append 
+"instal completed" | out-file c:/log.txt -append
+}
+
+}
+Catch
+{
+$ErrorMessage = $_.Exception.Message
+$Time=Get-Date
+"This script failed at $Time and error message was $ErrorMessage" | out-file c:\log.txt -append
+}
