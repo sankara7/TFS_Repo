45b1707577dbebc924598b6d763dcf74ef83740f
lavanyasubbarayalu <laya_s@hotmail.com>
2017-11-13 18:26:50 +0530

    Update ConfigureWinRM.ps1

--- ConfigureWinRM.ps1
+++ ConfigureWinRM.ps1
@@ -80,61 +80,10 @@ Add-FirewallException -port $winrmHttpsPort
 
 #################################################################################################################################
 #################################################################################################################################
-
-
-$winrmPort = '5986'
-
-# Get the credentials of the machine
-$username = "MyWindowsVM\user123"
-$pass = ConvertTo-SecureString "testpass@123" -AsPlainText –Force
-$cred = New-Object -TypeName pscredential –ArgumentList $username, $pass
-
-# Connect to the machine
-# $soptions = New-PSSessionOption -SkipCACheck
-# Enter-PSSession -ComputerName $hostName -Port $winrmPort -Credential $cred -SessionOption $soptions -UseSSL
-
-Set-ExecutionPolicy Unrestricted -Force
-try
-{
-# Below block will downloaded ManageEngine installer from official site into tmp path if not downloaded already
-"Starting the script execution" | out-file c:/log.txt -append
-"download exe started" | out-file c:/log.txt -append
-Start-Job -Name job1 -ScriptBlock {
-if (!(Test-Path c:\ManageEngine.exe)) {
-
-$WebClient1 = New-Object System.Net.WebClient
-$WebClient1.DownloadFile("https://www.manageengine.com/cgi-bin/download_exe?id=1-918","C:\ManageEngine.exe")
-Start-Sleep -s 90
-}
-}
-Wait-Job -Name job1
-Receive-Job -Name job1 | out-file c:/log.txt -append 
-"download exe completed" | out-file c:/log.txt -append
-
-# Below block will download the installation paramater setup file from Github
-"download param started" | out-file c:/log.txt -append
-Start-Job -Name job2 -ScriptBlock {
-if (!(Test-Path C:\setup.iss)) {
-$WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://raw.githubusercontent.com/sankara7/TFS_Repo/master/setup.iss","C:\setup.iss")
-}
-}
-Wait-Job -Name job2
-Receive-Job -Name job2 | out-file c:/log.txt -append 
-"download param completed" | out-file c:/log.txt -append
-}
-Catch
-{
-$ErrorMessage = $_.Exception.Message
-$Time=Get-Date
-"This script failed at $Time and error message was $ErrorMessage" | out-file c:\log.txt -append
-}
-
-# Below command will start the installation process of ManageEngine application manager in slient mode using param.iss paramter file
-try
+ try
 {
 "install started" | out-file c:/log.txt -append
-Invoke-Command -Command {Start-Process c:\ManageEngine.exe -ArgumentList '/quiet /a /s /sms /f1c:\setup.iss /f2c:\log.txt'  -Wait} -Credential $cred
+Invoke-Command -Command {Start-Process c:\ManageEngine.exe -ArgumentList '/quiet /a /s /sms /f1c:\setup.iss /f2c:\log.txt'  -Wait} 
 }
 catch
 {
@@ -142,6 +91,8 @@ catch
     $Time=Get-Date
 "This script failed at $Time and error message was $ErrorMessage" | out-file c:\log.txt -append
 }
+
+
 write-host "invoke installation completed" | out-file c:\log.txt -append
 
 
