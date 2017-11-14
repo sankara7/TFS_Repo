868d7dd55b64d67b92c009944d5d55df95a470bf
sankara7 <30866209+sankara7@users.noreply.github.com>
2017-11-14 18:33:38 +0530

    Add files via upload

---
+++ Inst.ps1
@@ -0,0 +1,18 @@
+﻿Configuration Install 
+{ 
+    try
+{
+
+"install started" | out-file c:/log.txt -append
+Invoke-Command -Command {Start-Process c:\ManageEngine.exe -ArgumentList '/quiet /a /s /sms /f1c:\setup.iss /f2c:\log.txt'  -Wait}
+"install completed" | out-file  c:/log.txt -append
+
+}
+catch
+{
+    $ErrorMessage = $_.Exception.Message
+    $Time=Get-Date
+"This script failed at $Time and error message was $ErrorMessage" | out-file c:\log.txt -append
+} 
+
+} 
