fa3a3d225d14434a9ee53a0e5db04d7fcba36bc9
sankara7 <30866209+sankara7@users.noreply.github.com>
2017-12-15 17:29:58 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -5,6 +5,7 @@ apt-get install software-properties-common
 apt-add-repository ppa:ansible/ansible
 apt-get update
 apt-get install ansible
+apt-get install unzip
 
 wget https://github.com/sankara7/TFS_Repo/raw/master/Ansible1.zip
 unzip Ansible1.zip -d /tmp
