bc1de970ffb02ee000c86b2721e389900e379233
sankara7 <30866209+sankara7@users.noreply.github.com>
2017-12-15 15:56:05 +0530

    Add files via upload

---
+++ script1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt-get update
+apt-get install software-properties-common
+apt-add-repository ppa:ansible/ansible
+apt-get update
+apt-get install ansible
+
+wget https://github.com/sankara7/TFS_Repo/raw/master/Ansible1.zip
+unzip Ansible1.zip -d /tmp
+
+cd /tmp/Ansible1
+
+ansible-playbook Play_Apache.yml
